ca7de702cdd6ff1c68df09bbb4124f0bb8708725
Maftoul Omar <maftoul.omar@gmail.com>
2019-05-09 12:57:21 +0200

    initial commit

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ from utility import sort_by_date
 
 class Simulator:
     def __init__(self):
+        self.steps = []
         self.user_input = Input()
         self.gamma = self.user_input.gamma
         self.next_state_x_prime = None
@@ -12,11 +13,17 @@ class Simulator:
         self.initial_event_date_t_previous = 0.0
         self.next_event_date_t_prime = 0
         self.duration = self.user_input.set_of_durations
+        for event in self.duration:
+            self.steps.append("initial clock list for the {"+event+"}"+" event : " + str(self.duration[event]) + "\n")
+            self.steps.append("t' = " + str(self.initial_event_date_t_previous)+'\n')
         self.calender = []
         self.last_clock_pop = {}
         which_method = self.user_input.method_input()
         if which_method == "simple":
             self.simulate_simple_way()
+            file = open("C:/Users/omarm/Desktop/output.txt","w")
+            for s in self.steps:
+                file.write(s)
         elif which_method == "hard":
             self.simulate()
         for c in self.calender:
@@ -47,32 +54,47 @@ class Simulator:
 
     # without updating the clock value each time
     def simulate_simple_way(self):
-        counter = 0
+        counter = 1
         ordered_events_by_date = {}
         self.calender.append(
             {'event': None, 'next_state': self.user_input.state_machine['initial_state'],
              'clock': None, 'date': self.initial_event_date_t_previous})
         # if there we still have clock values on the durations list
         while self.still_more_clocks():
+            self.steps.append(str(counter) + " iteration :"+"\n"+"-------------"+"\n")
             # get the current state from the last added state from the calender dictionary
             current_state = self.calender[-1]['next_state']
             # get the possible active events by passing the current state inside the gamma function
             active_events = self.gamma[current_state]
             # looping through the active events
             for event in active_events:
-                if event not in ordered_events_by_date:
+                if not self.duration[event]:
+                    self.steps.append("{"+event+"}" +
+                                      " event is active, but all corresponding clocks are already used "+'\n')
+                if event not in ordered_events_by_date: # if the event is not already on the ordered list
                     if self.duration[event]:
-                        ordered_events_by_date[event] = self.initial_event_date_t_previous + self.duration[event].pop(0)
+                        clock = self.duration[event].pop(0)
+                        ordered_events_by_date[event] = self.initial_event_date_t_previous + clock
+                        self.steps.append("{"+event+"}" +
+                                      " event is active, "+"the corresponding clock '"+
+                                      str(clock) +"' will be used and added to the date " +
+                                      str(self.initial_event_date_t_previous)+ '\n')
+            for event in self.duration:
+                self.steps.append("clock list for the {"+str(event)+"} "+" event : " +str(self.duration[event])+"\n")
             ordered_events_by_date = sort_by_date(ordered_events_by_date)
+            self.steps.append("ordered list " +str(ordered_events_by_date) + '\n')
             self.next_event_e_prime = next(iter(ordered_events_by_date))
+            self.steps.append("the {"+self.next_event_e_prime+"}"+" event is the trigger event, it well be removed from the ordered list\n")
             self.next_event_date_t_prime = ordered_events_by_date[self.next_event_e_prime]
             self.initial_event_date_t_previous = self.next_event_date_t_prime
+            self.steps.append("t' = " + str(self.initial_event_date_t_previous)+"\n")
             self.next_state_x_prime = self.next_state(current_state, self.next_event_e_prime)
             ordered_events_by_date.pop(self.next_event_e_prime)
             events = list(ordered_events_by_date.keys())
             for event in events:
                 if not self.still_active(event,self.next_state_x_prime):
                     ordered_events_by_date.pop(event)
+                    self.steps.append("the event {" +str(event) +"} will be removed because it's not active in the next state"+ '\n')
             self.calender.append(
                 {'event': self.next_event_e_prime, 'next_state': self.next_state_x_prime,
                  'clock': None, 'date': self.next_event_date_t_prime})
