949cbe0fc952ed7f29b00c28916389f01156fe2f
Maftoul Omar <maftoul.omar@gmail.com>
2019-05-19 19:10:17 +0200

    initial commit

--- EngineRun.py
+++ EngineRun.py
@@ -30,19 +30,19 @@ class Example(Frame):
             calendar = model.calendar
         canvas = Canvas(self)
         steps = {}
-        canvas.create_line(20, 200, 970, 200)
+        canvas.create_line(20, 200, 1070, 200)
         i = 20
         step = -0.1
-        while i <= 970:
+        while i <= 1070:
             step +=0.1
-            checker = step/0.5
-            if step >= 1:
-                    if round(checker,2) % int(checker) == 0: # draw step only if it's one step or half step
-                        canvas.create_line(i,205,i,195)
+            # checker = step/0.5
+            # if step >= 1:
+            #         if round(checker,2) % int(checker) == 0: # draw step only if it's one step or half step
+            #             canvas.create_line(i,205,i,195)
             if step == 0.0 or step == 0.5:
                 canvas.create_line(i,205,i,195)
             steps[round(step,1)] = [i,205,195]
-            i += 15.5
+            i += 5
 
         for i,c in enumerate(calendar):
                 current_event_date = calendar[i]['date']
@@ -52,11 +52,11 @@ class Example(Frame):
                 else:
                     state_x_position = (steps[current_event_date][0] + 35)
                 state_y_position = 185
-                canvas.create_text(state_x_position, state_y_position, text = 'state = ' + str(c['next_state']), font='Helvetica 9 bold')
+                canvas.create_text(state_x_position, state_y_position, text = str(c['next_state']), font='Helvetica 8 bold')
                 if c['event'] is not None :
                     canvas.create_line(steps[current_event_date][0],190,steps[current_event_date][0],160, arrow = tk.FIRST)
-                    canvas.create_text(steps[current_event_date][0],150, text= 'event ' + str(i) +" = " + str(c['event']), font='Helvetica 9 bold')
-                    canvas.create_text(steps[current_event_date][0],215, text = str(current_event_date), font ='Helvetica 9 bold')
+                    canvas.create_text(steps[current_event_date][0],150, text= str(c['event']), font='Helvetica 8 bold')
+                    canvas.create_text(steps[current_event_date][0],215, text = str(current_event_date), font ='Helvetica 8 bold')
                     canvas.create_line(steps[current_event_date][0],205,steps[current_event_date][0],195)
         y = 360
         for state in model.gamma:
@@ -77,7 +77,7 @@ def main():
 
     root = Tk()
     ex = Example()
-    root.geometry("1000x600")
+    root.geometry("1600x600")
     root.mainloop()
 
 
