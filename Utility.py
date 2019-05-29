ab3e90ec0594bd68be9aaa82a2090ec06c8fcd9d
Maftoul Omar <maftoul.omar@gmail.com>
2019-05-29 13:37:12 +0200

    initial commit

--- Utility.py
+++ Utility.py
@@ -1,31 +1,67 @@
+# Utility class offers a number of functions
+# each function has it's own task and runs a particular algorithm useful in another class
+
 import numpy as np
 MAX_DIMENSION = 3
 TYPO_ERROR = 27
 NON_RANDOM = 0
 RANDOM = 1
 
+# this function is responsible for asking the user to enter the necessary input
+# it's useful for FINITE MODEL'S SIMULATION ONLY
+# because we're asking the user to give multiple information about the model he wants to simulate
+# we'll call this function multiple times in the Model class,
+# hence, instead of repeating the code, we'll use this function with two parameters
+# the first parameter is the string message which should be displayed to ask the user of the number of states or the number of events in his model
+# the second parameter asks the user to enter the enter the data related to the model, i.e : enter the names of the events, or the name of states
+# note that because we're asking the user of the number of states and events, we're about simulating a FINITE AUTOMATON, the input of the Infinite systems
+# is on the InfiniteModelsInput Class
+
 def asking_for_input(ask_for_number_of_data, ask_for_data):
+    # Handling particular exceptions that may occurs if the user enters an invalid input
     try:
+        # ask the user to put the number of states or events and convert it to an integer because it's a number
         number_of_data = int(input(ask_for_number_of_data))
+        # because we use this function multiple times depending on the type of data we're asking the user to put,
+        # we check if we're asking the user about the states or events
+        # so if we're asking about the number of states in the user's model
         if "states" in ask_for_number_of_data:
+            # if the number of states entered previously by the user is less than or equal to 1
             while number_of_data <= 1:
+                # we ask the user to put a valid number over and over again until he enter a valid number of states
+                # for example : we can't have an automaton with one state (maybe we can but it's useless)
                 print("invalid model, try again")
                 number_of_data = int(input(ask_for_number_of_data))
+        # if we're asking the user about the events
         if "events" in ask_for_number_of_data:
+            # if the user has entered a number of events that equal to 0
             while number_of_data == 0:
+                # we ask again until he enter a valid number
+                # automaton with 0 event seems to be useless
                 print("invalid model, try again")
                 number_of_data = int(input(ask_for_number_of_data))
-        data = sorted(list(dict.fromkeys(input(ask_for_data).split(','))))  # sorting the input in natural order, then convert the input to a list, then remove duplicate values
+        # data is a variable that represents the user's input after providing the number of data, it may be states or  events
+        # data is converted to a list, i.e : if the user provides 3 as number of states, then data will hold 3 elements ( if of course the user puts consistent input )
+        # sorting the input in natural order, then convert the input to a list, then remove duplicate values ( because an automaton has unique states and unique events)
+        data = sorted(list(dict.fromkeys(input(ask_for_data).split(','))))
+        # check if the user have a consistent input
+        # i.e : if the given number of events was 3 (a,b,c) and the user has provided only two events (a,b), or the user
+        # has provided an empty string by hitting enter
+        # then the statement below is true
         while len(data) < number_of_data or len(data) > number_of_data or "" in data or " " in data:
+            # as we do every time, we ask for a valid input while the user's provided a wrong input
             print('invalid input, try again')
             data = sorted(list(dict.fromkeys(input(ask_for_data).split(','))))
+    # catch the eventual exception that we may encounter ( ValueError exception for eventual non integer input in when asking for number of data )
+    # i.e : the user gave a comma ',', the program tries to cast it into an integer, it will fails and rise a ValueError Exception
     except None or ValueError or SyntaxError:
         print('invalid input, try again')
         return None
+    # return the data (list of states or events ) to the calling function
     return data
 
 # sorting the dictionary that contains events and its corresponding dates by dates
-# keys are events, t are values
+# keys are events, dates are values
 # i.e : { 'a':0.5, 'b':1.5 }
 def sort_by_date(dictionary):
     ordered = sorted(dictionary.items(), key = lambda event__date: (event__date[1], event__date[0]))
@@ -35,6 +71,7 @@ def sort_by_date(dictionary):
     return dictionary
 
 
+# this is the function that helps us
 def asking_for_input_infinite_models():
     ask_for_file = input("Put the file that contains model description : ")
     try:
