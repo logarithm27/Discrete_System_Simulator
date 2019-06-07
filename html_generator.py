b74c0c0998f1d16396f0ac29d903d57320e0c64c
Maftoul Omar <maftoul.omar@gmail.com>
2019-06-07 13:37:40 +0200

    initial commit

--- html_generator.py
+++ html_generator.py
@@ -5,6 +5,3 @@ HTML = ["<head>" + "\n","<title>Timing Graph</title>\n", "<!-- Plotly.js -->" + 
         "<div style = 'font-family: Tahoma, Geneva, sans-serif;font-size: 26px;letter-spacing: 0px;word-spacing: 3px;color: #3C3B3B;font-weight: 400;text-decoration: none;font-style:normal;font-variant: normal;text-transform: none; margin-left:140px; margin-bottom:6px;'>SIMUL</div>",
         "<div id='probabilities' style='width: 100%; height: 500px;'></div>\n",
         "<script>"]
-if __name__ == '__main__':
-    for line in HTML:
-        print(HTML[7])
